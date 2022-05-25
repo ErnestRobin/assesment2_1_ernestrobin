076b662e6801bf06a699d1de4c4b6ae491f30b11
ErnestRobin <ernet.litsa.ac@st.vvu.edu.gh>
2022-05-25 13:31:35 +0000

    first commit

---
+++ bin/assessemnt2_1_ernestrobin.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+// this assessment 2 question 1. the program disply the name, favorite app and city
+  var name = "Ernest Litsa";
+  var favoriteApp = "facebook";
+  var city = "Accra";
+  print(
+      'My name is ${name} and my favorite app is ${favoriteApp} and I lived in ${city} Ghana');
+}
